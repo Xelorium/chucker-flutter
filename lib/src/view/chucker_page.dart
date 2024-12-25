1eb36cc48f554175900e78ac3ffd031f47be1253
Muhammed Bayraktar <mhmmd.bayraktar@gmail.com>
2024-12-25 17:16:09 +0300

    Added open folder to success dialog.

--- lib/src/view/chucker_page.dart
+++ lib/src/view/chucker_page.dart
@@ -16,6 +16,7 @@ import 'package:chucker_flutter/src/view/widgets/menu_buttons.dart';
 import 'package:chucker_flutter/src/view/widgets/stats_tile.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:open_filex/open_filex.dart';
 import 'package:path_provider/path_provider.dart';
 
 ///The main screen of `chucker_flutter`
@@ -337,16 +338,17 @@ class _ChuckerPageState extends State<ChuckerPage> {
             '-',
           );
 
-      final filePath = '${directory.path}/$fileName-$date.csv';
+      final filePath = '${directory.path}/${fileName}_$date.csv';
 
       // Write the CSV data to the file
       final file = File(filePath);
       await file.writeAsString(csvData);
 
       if (kDebugMode) {
-        print('CSV file saved at: $filePath');
+        print('CSV file saved at: file://$filePath');
+        print('Folder: file://${directory.path}');
       }
-      await showResultPopUp(isSuccess: true, message: 'CSV file saved at: $filePath');
+      await showResultPopUp(isSuccess: true, message: 'CSV file saved at: $filePath', fileLocation: filePath);
     } catch (e, stackTrace) {
       if (kDebugMode) {
         print('Error exporting CSV: $e\n$stackTrace');
@@ -355,7 +357,9 @@ class _ChuckerPageState extends State<ChuckerPage> {
     }
   }
 
-  Future<void> showResultPopUp({required bool isSuccess, required String message}) async {
+  Future<void> showResultPopUp({required bool isSuccess, required String message, String? fileLocation}) async {
+    if ((fileLocation ?? '').isEmpty) return;
+
     return showDialog(
         context: context,
         builder: (context) {
@@ -363,6 +367,14 @@ class _ChuckerPageState extends State<ChuckerPage> {
             title: Text(isSuccess ? 'Success' : 'Failed'),
             content: Text(message),
             actions: <Widget>[
+              //open folder for ios
+              if (Platform.isIOS)
+                TextButton(
+                  child: const Text('Open Folder'),
+                  onPressed: () async {
+                    await openFileOrFolder(fileLocation!);
+                  },
+                ),
               TextButton(
                 child: const Text('Ok'),
                 onPressed: () {
@@ -372,6 +384,22 @@ class _ChuckerPageState extends State<ChuckerPage> {
             ],
           );
         });
+  }
+
+  Future<void> openFileOrFolder(String filePath) async {
+    try {
+      // Use open_filex to open the file in the system's default app
+      final result = await OpenFilex.open(filePath);
+      if (result.type != ResultType.done) {
+        if (kDebugMode) {
+          print('Failed to open file: ${result.message}');
+        }
+      }
+    } catch (e) {
+      if (kDebugMode) {
+        print('Error opening file: $e');
+      }
+    }
   }
 
   Future<void> _deleteAllSelected() async {
